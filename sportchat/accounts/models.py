97572a0c306b4e97e141813d62256f6634ad1801
VidyaYadavv1519 <vidyayadav617@gmail.com>
2023-10-06 00:10:41 +0530

    Created a User model in the appropriate app

--- sportchat/accounts/models.py
+++ sportchat/accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    # Add custom fields here
+    Name = models.CharField(max_length=200,blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    bio = models.TextField(blank=True)
+
+    # Add any other custom fields you need
+
+    def __str__(self):
+        return self.username
+
+
