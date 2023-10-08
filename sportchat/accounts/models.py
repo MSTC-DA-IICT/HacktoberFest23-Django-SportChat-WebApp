24653fd3d3b71bc761d84896dc7b6b77ff9d3a26
PavankumarUppar <pavankumar.ai20@bmsce.ac.in>
2023-10-08 11:57:03 +0530

    Implemented the user creation form in the backend

--- sportchat/accounts/models.py
+++ sportchat/accounts/models.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 class CustomUser(AbstractUser):
-    # Add custom fields here
-    Name = models.CharField(max_length=200,blank=True, null=True)
-    date_of_birth = models.DateField(blank=True, null=True)
-    bio = models.TextField(blank=True)
+    # Fields from the registration form
+    email = models.EmailField(unique=True)  # Email address
+    first_name = models.CharField(max_length=150)  # Name
+    password1 = models.CharField(max_length=128)  # Password (hashed)
+    password2 = models.CharField(max_length=128)  # Password confirmation (hashed)
+    contact_number = models.CharField(max_length=15)  # Contact Number
 
     # Add any other custom fields you need
 
     def __str__(self):
         return self.username
-
-
--- sportchat/accounts/views.py
+++ sportchat/accounts/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
+from django.shortcuts import render, redirect
+from .forms import CustomUserRegistrationForm
 
 def register(request):
+    if request.method == 'POST':
+        form = CustomUserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to a success page or login page
+            return redirect('login')  # Change 'login' to your login URL name
+    else:
+        form = CustomUserRegistrationForm()
 
-    return render(request,'register.html')
-
+    return render(request, 'register.html', {'form': form})
