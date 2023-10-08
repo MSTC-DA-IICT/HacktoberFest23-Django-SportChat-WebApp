a364e17e897547db31ed133f4dbe80fdaff95cc4
VidyaYadavv1519 <vidyayadav617@gmail.com>
2023-10-08 14:26:27 +0530

    Implemented Group Model

--- sportchat/groups/models.py
+++ sportchat/groups/models.py
@@ -1,5 +1,21 @@
-
 from django.db import models
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    group_description = models.TextField()
+    members = models.ManyToManyField('Member', related_name='groups')
+
+    def __str__(self):
+        return self.name
+
+class Member(models.Model):
+    name = models.CharField(max_length=255)
+    # You can add more fields for the member if needed
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
 
